f2c7bf75cc35f4f4c07b37c0e98d29ba7edfa121
Marx Medeiros <analista.marx@gmail.com>
2017-04-07 19:19:06 -0300

    Importação de Associado
    Importação de Associado

--- init.php
+++ init.php
@@ -24,6 +24,9 @@ function dbcon()
     @mysql_select_db("siged_novo") or die(mysql_error());
 }
 
+//CONEXÃO PARA IMPORTAÇÃO
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
+$db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
 
 
 
